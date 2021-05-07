9e542d5d25e8c713f4d302109c7d7d4c6497d8da
wangbin <wangbin@163.com>
2021-05-07 23:05:59 +0800

    "第一次修改"

--- project/src/com/company/Main.java
+++ project/src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello git!");
-        System.out.println("hello git!123456");
+//        System.out.println("hello git!123456");
     }
 }
